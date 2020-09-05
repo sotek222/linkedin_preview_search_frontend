408944531583f6cba89c51420c99196069b53518
Matthew Masiello <sotek222@yahoo.com>
2020-09-05 19:21:39 -0400

    add initial file structure

---
+++ src/components/SearchForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function SearchForm(){
+
+  return (
+    <form>
+      <input type="text" placeholder="https://www.linkedin.com/in/username"/>
+      <button type="submit">Submit</button>
+    </form>
+  )
+};
+
+export default SearchForm;
