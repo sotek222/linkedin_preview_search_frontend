00d6b508de393eba4383dc1aae91cfb5d3d15a0f
Matthew Masiello <sotek222@yahoo.com>
2020-09-06 13:30:09 -0400

    add comments

--- src/util/validator.js
+++ src/util/validator.js
@@ -1,4 +1,9 @@
-function validateUrl(url){
+
+/**
+ * Validates url using a regex pattern to determine if it is valid and returns a boolean
+ * @param {string} url - a string representing a URL inputed from the user
+ */
+const validateUrl = (url) => {
   const regexPattern = /^(https:\/\/www.)(linkedin).com\/in\//;
   return regexPattern.test(url);
 };
