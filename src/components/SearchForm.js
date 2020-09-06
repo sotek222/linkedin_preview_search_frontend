b874f4e4a3dcc9a6278616b7b843c5641a766b27
Matthew Masiello <sotek222@yahoo.com>
2020-09-06 13:02:46 -0400

    add more comments

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,17 +1,30 @@
 import React, { useState } from 'react';
 
+/**
+ * renders the search form 
+ * @param {Object} props - A property object passed down from App.js
+ * @param {function} props.getLinkedInPreview - A function for handling user input after clicking submit
+ */
 function SearchForm({ getLinkedInPreview }){
   // used to store the form input value, and update the state of the input.
   const [searchInputValue, setSearchInputValue] = useState("");
 
   // changes searchInputValue to hold the value that the user inputs.
+  /**
+   * updates state so the input is controlled programmatically 
+   * @param {Object} event - An object that contains data about what the user inputed  
+   */
   const handleSearchInputChange = (event) => setSearchInputValue(event.target.value);
 
-  // handles the submit event and cleans up the form
+  /**
+   * handles the submit event and cleans up the form
+   * @param {*} event - An object that is used to override the default submit behavior
+   */
   const handleSubmit = (event) => {
     // prevents the form from making a post request and refreshing the page
     event.preventDefault();
     getLinkedInPreview(searchInputValue);
+    // clear the form for future input
     setSearchInputValue("");
   };
 
